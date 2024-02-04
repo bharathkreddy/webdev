c18ece2374d1b686bff6547e6afd36a48f613f30
bharathkreddy <reddybharath.k@gmail.com>
2024-02-04 18:16:39 +0000

    jquery to capture keypress added

--- static/JS/jq.js
+++ static/JS/jq.js
@@ -1,1 +1,18 @@
-$("li").css("color", "black");
+$("button").click(
+    function fn() {
+        if ($("button").text() === "hide") {
+            $("li").addClass("hidden");
+            $("button").text("unhide");
+        } else {
+            $("li").removeClass("hidden");
+            $("button").text("hide");
+        }
+    }
+)
+
+$(document).on(
+    "keypress",
+    function fn(e){
+        $(".userInput").text(e.key);
+    }
+)
